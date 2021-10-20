7c1a98d8a29fb2fbb57801c558f38032c67a8c99
Namik Klapic <namiklapic@gmail.com>
2021-10-20 15:46:10 +0200

    Add user authorization.

--- src/main/java/com/howtographql/hackernews/Mutation.java
+++ src/main/java/com/howtographql/hackernews/Mutation.java
@@ -1,18 +1,34 @@
 package com.howtographql.hackernews;
 
 import com.coxautodev.graphql.tools.GraphQLRootResolver;
+import graphql.GraphQLException;
 
 public class Mutation implements GraphQLRootResolver {
 
     private final LinkRepository linkRepository;
+    private final UserRepository userRepository;
 
-    public Mutation(LinkRepository linkRepository) {
+    public Mutation(LinkRepository linkRepository, UserRepository userRepository) {
         this.linkRepository = linkRepository;
+        this.userRepository = userRepository;
     }
 
     public Link createLink(String url, String description) {
         Link newLink = new Link(url, description);
         linkRepository.saveLink(newLink);
         return newLink;
+    }
+
+    public User createUser(String name, AuthData authData) {
+        User newUser = new User(name, authData.getEmail(), authData.getPassword());
+        return userRepository.saveUser(newUser);
+    }
+
+    public SigninPayload signinUser(AuthData auth) throws IllegalAccessException {
+        User user = userRepository.findByEmail(auth.getEmail());
+        if (user.getPassword().equals(auth.getPassword())) {
+            return new SigninPayload(user.getId(), user);
+        }
+        throw new GraphQLException("Invalid credentials");
     }
 }
